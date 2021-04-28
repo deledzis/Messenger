e499c79e0ecf496fa9c988a90a6e840251e781d1
deledzis <sstyagov2@gmail.com>
2021-04-28 04:15:50 +0300

    Fix pipeline

--- common/src/main/java/com/deledzis/messenger/common/Constants.kt
+++ common/src/main/java/com/deledzis/messenger/common/Constants.kt
@@ -1,7 +1,7 @@
 package com.deledzis.messenger.common
 
 object Constants {
-    const val MOCK_BUILD: Boolean = false
+    const val MOCK_BUILD: Boolean = true
     private const val API_VERSION: String = "v1"
     const val API_BASE_URL_MOCK: String = "http://10.0.2.2:8080/$API_VERSION/"
     const val API_BASE_URL_PROD: String = "https://spbstu-messenger.herokuapp.com/$API_VERSION/"
